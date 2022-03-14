92d75bd55dc35fa013ee0b0b1b41e52aa8b86e52
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-03-14 11:16:00 -0500

    we can enter orders and rest too!

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -16,6 +16,25 @@
 
     }
 
+    FormHandler.prototype.addSubmitHandler = function (func) {
+        console.log('Setting the submit handler for the form...');
+
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            console.log(data);
+            func(data);
+
+            this.reset();
+            this.elements[0].focus();
+        });
+    }
+
     App.FormHandler = FormHandler;
     window.App = App;
 
