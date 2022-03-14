d132dd595be1dfefcef164265c3c25c5d4eccb5e
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-03-14 10:44:20 -0500

    we added a selector that does not make unnecessary trips to the DOM

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -2,8 +2,17 @@
     'use strict';
 
     let App = window.App || {};
+    let $ = window.jQuery;
 
-    function FormHandler() {
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided!')
+        }
+
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find the element with selector: ' + selector);
+        }
 
     }
 
