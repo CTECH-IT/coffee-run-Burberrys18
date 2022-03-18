4b7d8a9644a47a2f8b956e0ea4a4b697cb0eb9a2
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-03-18 11:32:07 -0500

    made the coffee orders go to the remote DS server          &   finished the coffee run!!!

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,19 @@
     const FORM_SELECTOR = '[data-coffee-order="form"]';
     const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
 
+    const SERVER_URL = 'http://saturn.rochesterschools.org:8080/json';
+
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore; 
+    let RemoteDataStore = App.RemoteDataStore;
     let FormHandler = App.FormHandler;
     let Checklist = App.Checklist;
     let Validation = App.Validation;
 
-    let myTruck = new Truck('12345', new DataStore());
+    let remoteDS = new RemoteDataStore(SERVER_URL);
+
+    let myTruck = new Truck('12345', remoteDS);
     window.myTruck = myTruck;
 
     let checklist = new Checklist(CHECKLIST_SELECTOR);
